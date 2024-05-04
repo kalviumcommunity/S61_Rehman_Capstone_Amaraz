5be7cac3d952bcfe0456be4b33cec86aacdd5364
Habeeb <skhabeebhussain91@.com>
2024-05-04 15:30:42 +0530

    Basic post request

--- Backend/server.js
+++ Backend/server.js
@@ -1,12 +1,18 @@
+const e = require('express');
 const express = require('express');
 const app = express();
 const port = 3000;
 const mongoose = require('mongoose')
 require('dotenv').config()
 
+app.use(express.json())
 app.get('/', (req, res) => {
     res.send('Hello World!');
 });
+
+app.post('/post',(req , res) =>{
+    res.send(req.body);
+})
 
 app.listen(port, () => {
     console.log(`Server is running on port ${port}`);
